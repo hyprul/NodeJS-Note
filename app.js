4b7a0ac39eefa44e0695b345ed49a98b19d23097
Sean Huang <hyprul@ucla.edu>
2018-11-27 15:30:19 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+console.log("Starting app.js");
+
+const fs = require("fs");
+const os = require("os");
+const notes = require("./notes.js");
+
+// var res = notes.addNote();
+// console.log(res);
+
+var add = notes.add(3,5);
+console.log(add);
+
+// var user = os.userInfo();
+
+// fs.appendFileSync("greetings.txt", `Hello ${user.username}! You are ${notes.age}.`);
