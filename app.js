b1de8e7a7240c3ef10cab2300a4d288f3ffc71af
Sean Huang <hyprul@ucla.edu>
2018-11-28 16:32:59 -0600

    npm

--- app.js
+++ app.js
@@ -2,13 +2,21 @@ console.log("Starting app.js");
 
 const fs = require("fs");
 const os = require("os");
+const _ = require('lodash');
 const notes = require("./notes.js");
+
+// console.log(_.isString(true));
+// console.log(_.isString("Andrew"));
+
+var filteredArray = _.uniq([2,3,4,1,2,3,4,10])
+console.log(filteredArray)
+
 
 // var res = notes.addNote();
 // console.log(res);
 
 var add = notes.add(3, 5);
-console.log(add);
+console.log('Result:', add);
 
 var subtract = notes.subtract(8, -2);
 console.log(subtract);
