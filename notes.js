3b3691c91e88bb17eaf358754cd3976b23b5c101
Sean Huang <hyprul@ucla.edu>
2018-11-30 14:35:38 -0600

    add note

--- notes.js
+++ notes.js
@@ -1,23 +1,33 @@
 console.log("Starting notes.js");
 
+const fs = require("fs");
 // module.exports.addNote = () => {
 // 	console.log("addNote");
 // 	return "New note";
 //
 
 var addNote = (title, body) => {
-	console.log("Adding note", title, body);
+	//console.log("Adding note", title, body);
+	var notes = [];
+	var note = {
+		title,
+		body
+	};
+
+	notes.push(note);
+
+	fs.writeFileSync("notes-data.json", JSON.stringify(notes));
 };
 
 var getAll = () => {
 	console.log("Getting all notes");
 };
 
-var getNote = (title) => {
+var getNote = title => {
 	console.log("Getting note", title);
 };
 
-var removeNote = (title) => {
+var removeNote = title => {
 	console.log("Removing note", title);
 };
 module.exports = { addNote, getAll, getNote, removeNote };
