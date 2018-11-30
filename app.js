83327daa31edf53c3d846777be9f43ce40d263e6
Sean Huang <hyprul@ucla.edu>
2018-11-30 13:48:30 -0600

    commands

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 console.log("Starting app.js");
 
 const fs = require("fs");
-const _ = require('lodash');
+const _ = require("lodash");
 
 const notes = require("./notes.js");
+
+var command = process.argv[2];
+
+console.log("Command: ", command);
+
+if (command === "add") {
+	console.log("Adding new note");
+} else if (command === "list") {
+	console.log("Listing all notes");
+} else {
+	console.log("Command not recognized");
+}
